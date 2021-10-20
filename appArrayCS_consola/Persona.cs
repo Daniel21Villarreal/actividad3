a50d24bb50c091c472de0b0ffbb8894c8ef49167
Daniel21Villarreal <92354716+Daniel21Villarreal@users.noreply.github.com>
2021-10-19 20:29:53 -0500

    Add files via upload

---
+++ appArrayCS_consola/Persona.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace appArrayCS_consola
+{
+    class Persona
+    {
+        private string nombre;
+        private string apellidos;
+        private DateTime? fechaNacimiento;
+        public string Apellidos
+        {
+            get { return apellidos; }
+            set { apellidos = value; }
+        }
+
+
+        public DateTime FechaNacimiento
+        {
+            get { return FechaNacimiento; }
+            set { fechaNacimiento = value; }
+        }
+
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+
+        public Persona()
+        {
+            nombre = "";
+            apellidos = "";
+            fechaNacimiento = null;
+        }
+        public Persona(string nombre, string apellidos, DateTime? fechaNacimiento)
+        {
+            this.nombre = nombre;
+            this.apellidos = apellidos;
+            this.fechaNacimiento = fechaNacimiento;
+        }
+        public override string ToString()
+        {
+            return nombre + " " + apellidos + " - " + fechaNacimiento;
+        }
+
+    }
+ }
+   
+
---
+++ appArrayCS_consola/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace appArrayCS_consola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Arreglo de 10 string´s
+            //string[] a = new string[10]; 
+
+            //a[0] = "algo";
+            //a[1] = "aaass";
+            //for (int i = 0; i < a.Length; i++)
+            // {
+            //a[i] = "hola" + (i + 1);
+            // }
+            //for  (int i =0; i < a.Length; i++)
+            // {
+            // Console.WriteLine(a[i]);
+            // }
+            Persona[] arregloPersonas = new Persona[5];
+            for (int i =0;i < arregloPersonas.Length; i++)
+            {
+                arregloPersonas[i] = new Persona();
+                arregloPersonas[i].Nombre = "";
+                Console.WriteLine("INGRESA NOMBRE");
+                Console.ReadLine();
+                arregloPersonas[i].Apellidos = "";
+                Console.WriteLine("INGRESA A´PELLIDOS");
+                Console.ReadLine();
+                arregloPersonas[i].FechaNacimiento = new DateTime(2003, 8, 21);
+            }
+
+            for (int i = 0; i < arregloPersonas.Length; i++)
+            
+                Console.WriteLine( arregloPersonas[i].ToString());
+
+            Console.ReadKey();
+        }
+    }
+}
